3e9366c1894079ae6006afc7784be385aea67a93
Saylee's MacBook <mac@Saylees-MacBook-Air.local>
2025-10-29 19:51:50 +0530

    chore: remove unused insights app and posts templates; clean settings

--- insights/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import SocialPost
-
-
-@admin.register(SocialPost)
-class SocialPostAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "author", "created_at")
-    search_fields = ("title", "body")
-    list_filter = ("author", "created_at")
